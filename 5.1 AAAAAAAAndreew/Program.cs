158f5bda4bfd3b5ba11d1d1b4b056a09b22a2b8f
tihon <tihon@DESKTOP-ACP6SLU>
2021-12-21 11:51:02 +0200

    Добавьте файлы проекта.

---
+++ 5.1 AAAAAAAAndreew/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1_AAAAAAAAndreew
+{
+    class Program
+    {
+        static string desat(int n)
+        {
+            var s = string.Empty;
+            while (n > 0)
+            {
+                s = (n % 2) + s;
+                n /= 2;
+            }
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в десятичной системе: ");
+            var n = int.Parse(Console.ReadLine());
+            var s = desat(n);
+            Console.WriteLine("{0} => {1}", n, s);
+            Console.ReadLine();
+        }
+    }
+}
+
